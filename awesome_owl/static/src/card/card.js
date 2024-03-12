6a7159dd9a3da791c71f01c37c58efe0131e83cf
luissiguencia <lsiguencia@multicsoft.com>
2024-03-11 22:29:28 -0500

    feat: Implementado boton para ocultar contenido vista

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,6 +1,15 @@
 /** @odoo-module **/
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
-    static template = "awesome_owl.card"
+    static template = "awesome_owl.card";
+
+    setup() {
+        this.state = useState({ show: true });
+    }
+
+    toogle_show_content() {
+        this.state.show = !this.state.show;
+    }
+
 }
