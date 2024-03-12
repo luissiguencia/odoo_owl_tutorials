77a7bfb2a5045d4adc55ea2aa49793a81b8143a0
luissiguencia <lsiguencia@multicsoft.com>
2024-03-11 22:53:39 -0500

    feat: Teoria Servicios

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,12 +3,19 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = {
         Layout
     }
+
+    setup() {
+        this.myService = useService("myService");
+        // do something with value
+     }
+    
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
---
+++ awesome_dashboard/static/src/service.js
@@ -0,0 +1,14 @@
+/** @odoo-module **/
+import { registry } from "@web/core/registry";
+
+const myService = {
+dependencies: ["notification"],
+start(env, { notification }) {
+   let counter = 1;
+   setInterval(() => {
+      notification.add(`Tick Tock ${counter++}`);
+   }, 5000);
+},
+};
+
+registry.category("services").add("myService", myService);
